2fdabfdfedc65afb1234051d02dfbb0011f2f1c3
adrianariaz118-alt <adrianariaz118@gmail.com>
2025-12-04 00:25:31 +0100

    feat: ajout script PowerShell

---
+++ script.ps1
@@ -0,0 +1,9 @@
+param(
+    [int]$A,
+    [int]$B
+)
+
+Write-Output "Addition : $($A + $B)"
+Write-Output "Soustraction : $($A - $B)"
+Write-Output "Multiplication : $($A * $B)"
+Write-Output "Division : $($A / $B)"
